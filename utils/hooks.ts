9d0c5810eb31091ece24ddd022761f111877d7db
Gál Péter <pepyta118@gmail.com>
2020-06-25 11:44:37 +0200

    Create hooks.ts

---
+++ utils/hooks.ts
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+
+export const componentDidMount = handler => {
+  return useEffect(() => {
+    return handler();
+  }, []);
+};
+
+export const componentDidUpdate = (handler, deps) => {
+  const isInitialMount = useRef(true);
+
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+
+      return;
+    }
+
+    return handler();
+  }, deps);
+};
